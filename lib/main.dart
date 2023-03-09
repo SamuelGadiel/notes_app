aaeda73fd32c05a4386052fdce5c4d2c4f9db007
SamuelGadiel <samuelgadiel@gmail.com>
2023-03-09 20:21:23 -0300

    add firebase auth to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
-import 'package:Notando/app/app_module.dart';
-import 'package:Notando/app/app_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:rx_notifier/rx_notifier.dart';
 
-void main() {
+import 'app/app_module.dart';
+import 'app/app_widget.dart';
+import 'app/core/firebase/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final firebaseApp = await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseAuth.instanceFor(app: firebaseApp);
+
+
+
+
   runApp(ModularApp(module: AppModule(), child: const RxRoot(child: AppWidget())));
 }
